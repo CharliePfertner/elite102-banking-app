48cede986c4b5e6ffc240325109ad2bbbec81a45
CharliePfertner <robotpfertner@gmail.com>
2026-04-21 17:50:52 -0500

    Added transaction history accessibility that is currently only availible for admins

--- main.py
+++ main.py
@@ -17,6 +17,17 @@ cursor.execute('''
     )
 ''')
 
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS transactions (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        account_id INTEGER,
+        type TEXT,
+        amount REAL,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (account_id) REFERENCES accounts(id)
+    )          
+''')
+
 # Step 3: Insert data
 cursor.execute("")
 conn.commit()   # IMPORTANT: saves your changes!
@@ -31,9 +42,9 @@ def input_name():
         if rows[i][1] == name:
             global reference
             reference = i
-            print("Name found")
+            print("\nName found!\n")
             return True
-    print("Please enter a name already in the database")
+    print("\nPlease enter a name already in the database\n")
 
 def input_password():
     password = input("Enter your password: ")
@@ -69,13 +80,15 @@ while logged_in != True:
 
 python_balance = rows[reference][2]
 python_id = rows[reference][0]
+status = None
 
 if python_id == 10:
     while logged_in == True:
         print("\n=== Admin Panel ===")
         print("1. Remove Account")
         print("2. List Accounts in Database")
-        print("3. Exit")
+        print("3. Check Transaction History")
+        print("4. Exit")
         choice = int(input("\nEnter your choice: "))
         if choice == 1:
             account_to_remove = int(input("\nEnter the ID of the account you want to remove: "))
@@ -88,6 +101,11 @@ if python_id == 10:
             for i in range(len(rows)):
                 print(f"\nID: {rows[i][0]}\nName: {rows[i][1]}\n")
         if choice == 3:
+            cursor.execute("SELECT * FROM transactions")
+            rows = cursor.fetchall()
+            for i in range(len(rows)):
+                print(f"\nAccount ID: {rows[i][1]}\nType: {rows[i][2]}\nAmount: ${rows[i][3]:.2f}\nDate: {rows[i][4]}\n")
+        if choice == 4:
             print("\nThank you for using the program!")
             logged_in = False
 
@@ -102,12 +120,16 @@ while logged_in == True:
     if choice == 1:
         print(f"Amount: ${python_balance}")
     if choice == 2:
-        deposit_amount = int(input("Please enter your deposit amount: "))
+        status = "Deposit"
+        deposit_amount = int(input("\nPlease enter your deposit amount: "))
         python_balance += deposit_amount
         cursor.execute("UPDATE accounts SET balance=%s WHERE id=%s",(python_balance, python_id))
         conn.commit()
-        print(f"Done! Deposit added\nBalance now: ${python_balance}")
+        cursor.execute("INSERT INTO transactions (account_id, type, amount) VALUES (%s, %s, %s)",(python_id, status, deposit_amount))
+        conn.commit()
+        print(f"\nDone! Deposit added\nBalance now: ${python_balance}")
     if choice == 3:
+        status = "Withdrawal"
         withdraw_amount = int(input("Please enter your withdraw amount: "))
         if withdraw_amount > python_balance:
             print("\nYour withdraw amount is too high! Please enter a lower withdraw amount.")
@@ -115,7 +137,9 @@ while logged_in == True:
         python_balance -= withdraw_amount
         cursor.execute("UPDATE accounts SET balance=%s WHERE id=%s",(python_balance, python_id))
         conn.commit()
-        print(f"Done! Withdraw added\nBalance now: ${python_balance}")
+        cursor.execute("INSERT INTO transactions (account_id, type, amount) VALUES (%s, %s, %s)",(python_id, status, withdraw_amount))
+        conn.commit()
+        print(f"\nDone! Withdraw added\nBalance now: ${python_balance}")
     if choice == 4:
         print("Thank you for using the program! ")
         logged_in = False
